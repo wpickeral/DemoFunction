0d9b8dfeeafe7bfdafde05a9e9957d3c2aa5d179
William Pickeral <willpickeral@gmail.com>
2024-10-14 09:32:38 -0400

    post Person

--- DemoFunction/Demo.cs
+++ DemoFunction/Demo.cs
@@ -1,24 +1,27 @@
+using DemoFunction.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
-namespace DemoFunction
+namespace DemoFunction;
+
+public class Demo
 {
-    public class Demo
+    private readonly ILogger<Demo> _logger;
+
+    public Demo(ILogger<Demo> logger)
     {
-        private readonly ILogger<Demo> _logger;
+        _logger = logger;
+    }
 
-        public Demo(ILogger<Demo> logger)
-        {
-            _logger = logger;
-        }
+    [Function("Demo")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
 
-        [Function("Demo")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-        {
-            // AuthorizationLevel.Function does not work locally
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
+        var data = await JsonSerializer.DeserializeAsync<PersonModel>(req.Body);
+        // AuthorizationLevel.Function does not work locally
+        return new OkObjectResult(data);
     }
 }
---
+++ DemoFunction/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFunction.Models
+{
+    internal class Class
+    {
+    }
+}
